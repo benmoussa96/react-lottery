71d2c3cd4f050772221338535d109e037a9513b3
benmoussa96 <ghaiethbmoussa@gmail.com>
2021-11-21 20:26:21 +0100

    Adding enter functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [manager, setManager] = useState('');
   const [players, setPlayers] = useState([]);
   const [balance, setBalance] = useState('');
+  const [value, setValue] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     async function getManager() {
@@ -19,14 +21,48 @@ function App() {
       const balance = await web3.eth.getBalance(lottery.options.address);
       setBalance(balance);
     }
+
     getManager();
   });
+
+  const onEnter = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    setMessage('Waiting for transaction to be proccessed...');
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, 'ether')
+    });
+
+    setMessage('You have been entered!');
+  };
 
   return (
     <div>
       <h2>Lottery Contract</h2>
       <p>This contract is managed by {manager}.</p>
       <p>There are currently {players.length} people competing to win {web3.utils.fromWei(balance, 'ether')} ether!</p>
+
+      <hr />
+
+      <form onSubmit={onEnter}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter (.01 min): </label>
+          <input
+            value={value}
+            onChange={event => setValue(event.target.value)}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
+
+      <hr />
+
+      <h1>{message}</h1>
     </div>
   );
 }
